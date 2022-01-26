ba80ca7d3ee32d28c9b3740503145d5e8cf6196a
Austin Gosling <Austin.Gosling@gmail.com>
2022-01-26 15:38:18 +0100

    unknown changes 26 Jan 2022

---
+++ elpa/protobuf-mode-20200619.1742/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20200619.1742" "major mode for editing protocol buffers." 'nil :commit "7ad0fd4ed782ec6e7476d0f9197e49ae70cb5dd4" :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com") :keywords '("google" "protobuf" "languages"))
